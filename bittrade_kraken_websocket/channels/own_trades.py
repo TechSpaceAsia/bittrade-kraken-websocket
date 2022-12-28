d712a3f42463bcf8725816d1afc0c7a48cbbd82f
mat <matt@techspace.asia>
2022-12-28 10:22:18 +0800

    CP

--- bittrade_kraken_websocket/channels/own_trades.py
+++ bittrade_kraken_websocket/channels/own_trades.py
@@ -6,71 +6,73 @@ from bittrade_kraken_websocket.channels import ChannelName
 from bittrade_kraken_websocket.channels.payload import private_to_payload
 from bittrade_kraken_websocket.channels.subscribe import subscribe_to_channel
 
+
 class OwnTradesPayloadEntry(TypedDict):
     """
-        [
-        {
-          "TDLH43-DVQXD-2KHVYY": {
-            "cost": "1000000.00000",
-            "fee": "1600.00000",
-            "margin": "0.00000",
-            "ordertxid": "TDLH43-DVQXD-2KHVYY",
-            "ordertype": "limit",
-            "pair": "XBT/EUR",
-            "postxid": "OGTT3Y-C6I3P-XRI6HX",
-            "price": "100000.00000",
-            "time": "1560516023.070651",
-            "type": "sell",
-            "vol": "1000000000.00000000"
-          }
-        },
-        {
-          "TDLH43-DVQXD-2KHVYY": {
-            "cost": "1000000.00000",
-            "fee": "600.00000",
-            "margin": "0.00000",
-            "ordertxid": "TDLH43-DVQXD-2KHVYY",
-            "ordertype": "limit",
-            "pair": "XBT/EUR",
-            "postxid": "OGTT3Y-C6I3P-XRI6HX",
-            "price": "100000.00000",
-            "time": "1560516023.070658",
-            "type": "buy",
-            "vol": "1000000000.00000000"
-          }
-        },
-        {
-          "TDLH43-DVQXD-2KHVYY": {
-            "cost": "1000000.00000",
-            "fee": "1600.00000",
-            "margin": "0.00000",
-            "ordertxid": "TDLH43-DVQXD-2KHVYY",
-            "ordertype": "limit",
-            "pair": "XBT/EUR",
-            "postxid": "OGTT3Y-C6I3P-XRI6HX",
-            "price": "100000.00000",
-            "time": "1560520332.914657",
-            "type": "sell",
-            "vol": "1000000000.00000000"
-          }
-        },
-        {
-          "TDLH43-DVQXD-2KHVYY": {
-            "cost": "1000000.00000",
-            "fee": "600.00000",
-            "margin": "0.00000",
-            "ordertxid": "TDLH43-DVQXD-2KHVYY",
-            "ordertype": "limit",
-            "pair": "XBT/EUR",
-            "postxid": "OGTT3Y-C6I3P-XRI6HX",
-            "price": "100000.00000",
-            "time": "1560520332.914664",
-            "type": "buy",
-            "vol": "1000000000.00000000"
-          }
+      [
+      {
+        "TDLH43-DVQXD-2KHVYY": {
+          "cost": "1000000.00000",
+          "fee": "1600.00000",
+          "margin": "0.00000",
+          "ordertxid": "TDLH43-DVQXD-2KHVYY",
+          "ordertype": "limit",
+          "pair": "XBT/EUR",
+          "postxid": "OGTT3Y-C6I3P-XRI6HX",
+          "price": "100000.00000",
+          "time": "1560516023.070651",
+          "type": "sell",
+          "vol": "1000000000.00000000"
         }
-      ]
+      },
+      {
+        "TDLH43-DVQXD-2KHVYY": {
+          "cost": "1000000.00000",
+          "fee": "600.00000",
+          "margin": "0.00000",
+          "ordertxid": "TDLH43-DVQXD-2KHVYY",
+          "ordertype": "limit",
+          "pair": "XBT/EUR",
+          "postxid": "OGTT3Y-C6I3P-XRI6HX",
+          "price": "100000.00000",
+          "time": "1560516023.070658",
+          "type": "buy",
+          "vol": "1000000000.00000000"
+        }
+      },
+      {
+        "TDLH43-DVQXD-2KHVYY": {
+          "cost": "1000000.00000",
+          "fee": "1600.00000",
+          "margin": "0.00000",
+          "ordertxid": "TDLH43-DVQXD-2KHVYY",
+          "ordertype": "limit",
+          "pair": "XBT/EUR",
+          "postxid": "OGTT3Y-C6I3P-XRI6HX",
+          "price": "100000.00000",
+          "time": "1560520332.914657",
+          "type": "sell",
+          "vol": "1000000000.00000000"
+        }
+      },
+      {
+        "TDLH43-DVQXD-2KHVYY": {
+          "cost": "1000000.00000",
+          "fee": "600.00000",
+          "margin": "0.00000",
+          "ordertxid": "TDLH43-DVQXD-2KHVYY",
+          "ordertype": "limit",
+          "pair": "XBT/EUR",
+          "postxid": "OGTT3Y-C6I3P-XRI6HX",
+          "price": "100000.00000",
+          "time": "1560520332.914664",
+          "type": "buy",
+          "vol": "1000000000.00000000"
+        }
+      }
+    ]
     """
+
     cost: str
     fee: str
     margin: str
@@ -83,19 +85,20 @@ class OwnTradesPayloadEntry(TypedDict):
     type: str
     vol: str
 
-OwnTradesPayload = List[Dict[str, OwnTradesPayloadEntry]]
 
+OwnTradesPayload = List[Dict[str, OwnTradesPayloadEntry]]
 
 
 def to_own_trades_payload(message: List):
     return private_to_payload(message, OwnTradesPayload)
 
 
-def subscribe_own_trades(pair: str, messages: Observable[Dict | List]):
+def subscribe_own_trades(messages: Observable[Dict | List]):
     return compose(
         subscribe_to_channel(messages, ChannelName.CHANNEL_OWN_TRADES),
         operators.map(to_own_trades_payload),
     )
+
 
 __all__ = [
     "OwnTradesPayload",

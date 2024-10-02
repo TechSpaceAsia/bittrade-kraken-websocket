ec37f5fc0899ed90a0745a8181c02126f3a6c1b9
Yu Jin <adamlyj89@gmail.com>
2024-10-02 22:52:13 +0800

    update orderbook algo

--- bittrade_kraken_websocket/channels/order_book.py
+++ bittrade_kraken_websocket/channels/order_book.py
@@ -14,8 +14,8 @@ RawPrice = Tuple[str, str, str, str]
 
 @dataclass
 class BookPrice:
-    ask: List[Price]
-    bid: List[Price]
+    ask: Dict[str, Price]
+    bid: Dict[str, Price]
 
 InitialBookPrice = TypedDict('InitialBookPrice', {'as': List[RawPrice], 'bs': List[RawPrice]})
 
@@ -27,54 +27,75 @@ OrderBook = Tuple[int, InitialBookPrice | UpdateBookPrice, str, str]
 
 def to_order_book_payload(payload: OrderBook):
     book_price_list = payload[1]
-    book_price = BookPrice(ask=[], bid=[])
-    keys = ["as", "bs", "a", "b"]
-    for key in keys:
-      for price in book_price_list.get(key, []):
-          if key == "as" or key == "a":
-              book_price.ask.append({"price": price[0], "volume": price[1], "timestamp": price[2]})
-          else:
-              book_price.bid.append({"price": price[0], "volume": price[1], "timestamp": price[2]})
-    
-    return book_price
+    if type(payload[2]) == dict:
+        book_price_list.update(payload[2])
+    # book_price = BookPrice(ask=dict(), bid=dict())
+    # keys = ["as", "bs", "a", "b"]
+    # for key in keys:
+    #     for price in book_price_list.get(key, []):
+    #         current_price = price[0]
+    #         if key == "as" or key == "a":
+    #             book_price.ask.update(
+    #                 {
+    #                     current_price: {
+    #                         "price": price[0],
+    #                         "volume": price[1],
+    #                         "timestamp": price[2],
+    #                     }
+    #                 }
+    #             )
+    #         else:
+    #             book_price.bid.update(
+    #                 {
+    #                     current_price: {
+    #                         "price": price[0],
+    #                         "volume": price[1],
+    #                         "timestamp": price[2],
+    #                     }
+    #                 }
+    #             )
 
-def accumulate_book_price(acc: BookPrice, current: BookPrice) -> BookPrice:
-    if current.ask and len(acc.ask):
-        for price in current.ask:
-            for ask_index, current_price in enumerate(acc.ask):
-                if price["price"] == current_price["price"]:
-                    if not float(price["volume"]):
-                        acc.ask.pop(ask_index)
-                        break
-                    
-                    acc.ask[ask_index] = price
-                    break
+    return book_price_list
 
-                if price["price"] <= current_price["price"] and float(price["volume"]):
-                    acc.ask.insert(ask_index, price)
-                    break
-    elif not len(acc.ask):
-        acc.ask = current.ask
-    
-    if current.bid and len(acc.bid):
-        for price in current.bid:
-            for bid_index, current_price in enumerate(acc.bid):
-                if price["price"] == current_price["price"]:
-                    if not float(price["volume"]):
-                        acc.bid.pop(bid_index)
-                        break
+def accumulate_book_price(acc: BookPrice, current: InitialBookPrice | UpdateBookPrice) -> BookPrice:
+    ask_list = current.get("as") or current.get("a", [])
+    bid_list = current.get("bs") or current.get("b", [])
 
-                    acc.bid[bid_index] = price
-                    break
+    for item in ask_list:
+        price = item[0]
+        if float(item[1]):
+            acc.ask.update({
+                    price: {
+                    "price": price,
+                    "volume": item[1],
+                    "timestamp": item[2],
+                }
+            })
+        else:
+            if price in acc.ask:
+                acc.ask.pop(price)
 
-                if price["price"] >= current_price["price"] and float(price["volume"]):
-                    acc.bid.insert(bid_index, price)
-                    break
-    elif not len(acc.bid):
-        acc.bid = current.bid
+    for item in bid_list:
+        price = item[0]
+        if float(item[1]):
+            acc.bid.update(
+                {
+                    price: {
+                        "price": price,
+                        "volume": item[1],
+                        "timestamp": item[2],
+                    }
+                }
+            )
+        else:
+            if price in acc.bid:
+                acc.bid.pop(price)
+
+    acc.ask = dict(sorted(acc.ask.items(), key=lambda x: float(x[0]))[:10])
+    acc.bid = dict(sorted(acc.bid.items(), key=lambda x: float(x[0]), reverse=True)[:10])
 
     return acc
-    
+
 def subscribe_order_book(
     pair: str, messages: Observable[Dict | List], subscription_kwargs: Optional[Dict] = None
 ):
@@ -87,5 +108,5 @@ def subscribe_order_book(
             pair=pair,
         ),
         operators.map(lambda x: to_order_book_payload(cast(OrderBook, x))),
-        operators.scan(accumulate_book_price, BookPrice(ask=[], bid=[])),
+        operators.scan(accumulate_book_price, BookPrice(ask=dict(), bid=dict())),
     )
